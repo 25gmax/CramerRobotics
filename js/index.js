f4617e8b2dd30ba43834358fdde2994e92485daf
25gmax <55412802+25gmax@users.noreply.github.com>
2020-05-05 16:21:16 -0400

    mobile

--- js/index.js
+++ js/index.js
@@ -26,6 +26,15 @@ function includeHTML() { //Thanks w3!
     }
   }
 };
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+};
 var slideIndex = 0;
 showSlides();
 
