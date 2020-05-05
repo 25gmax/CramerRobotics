ae0a3e55e3e7995ad5aff28b76ea4b98f3b53e71
25gmax <55412802+25gmax@users.noreply.github.com>
2020-05-05 16:32:39 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -29,10 +29,10 @@ function includeHTML() { //Thanks w3!
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "navbar") {
+  if (x.className === "topnav") {
     x.className += " responsive";
   } else {
-    x.className = "navbar";
+    x.className = "topnav";
   }
 };
 var slideIndex = 0;
