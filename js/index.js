e9ebc24e223c15bfa45491da1bc2a3acedbf9f45
25gmax <55412802+25gmax@users.noreply.github.com>
2020-05-05 16:36:53 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ function includeHTML() { //Thanks w3!
       return;
     }
   }
-};
+}
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
@@ -34,18 +34,4 @@ function myFunction() {
   } else {
     x.className = "topnav";
   }
-};
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
