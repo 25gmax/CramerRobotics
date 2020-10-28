f22763ab552b19d690287c45d9d3edeb0b8f01e4
25gmax <55412802+25gmax@users.noreply.github.com>
2020-10-28 18:38:19 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -633,3 +633,56 @@ jQuery(function ($) {
         }
     })
 })
+/*----------------------------------------------
+7. Responsive Menu
+----------------------------------------------*/
+
+jQuery(function ($) {
+
+    'use strict';
+
+    function navResponsive() {
+
+        let navbar = $('.webnavbar .items');
+        let menu = $('#menu .items');
+
+        menu.html('');
+        navbar.clone().appendTo(menu);
+
+        $('.menu .icon-arrow-right').removeClass('icon-arrow-right').addClass('icon-arrow-down');
+    }
+
+    navResponsive();
+
+    $(window).on('resize', function () {
+        navResponsive();
+    })
+
+    $('.menu .dropdown-menu').each(function() {
+
+        var children = $(this).children('.dropdown').length;
+        $(this).addClass('children-'+children);
+    })
+
+    
+    $('.menu .webnav-item.dropdown').each(function() {
+
+        var children = $(this).children('.nav-link');
+        children.addClass('prevent');
+    })
+
+    $(document).on('click', '#menu .webnav-item .webnav-link', function (event) {
+
+        if($(this).hasClass('prevent')) {
+            event.preventDefault();
+        }
+
+        var nav_link = $(this);
+
+        nav_link.next().toggleClass('show');
+
+        if(nav_link.hasClass('smooth-anchor')) {
+            $('#menu').modal('hide');
+        }
+    })
+})
